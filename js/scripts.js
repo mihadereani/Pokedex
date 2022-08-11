d05c4d1368d6e76c39e869853b40ec3084274ea8
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-08-11 11:49:27 +0200

    adjust your CSS
    make things look nicer

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,5 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</br>');
 }
